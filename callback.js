c883181872ff8acd600e1e2caccf0f118e08dc5e
Akesh M <akeshm.21ece@kongu.edu>
2024-01-23 13:32:16 +0530

    v1

--- callback.js
+++ callback.js
@@ -6,6 +6,7 @@ function ramuSomu(welcomeMessage, callback) {
   }, 2000);
 }
 function miniAni() {
+  //while (1) {}
   console.log("Odi Poiru vanthudatha da thambi!!!!");
 }
 ramuSomu("Hello Ramu and Somu\nWelcome to Kongu Engineering College", miniAni);
